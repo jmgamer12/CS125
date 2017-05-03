99042621f874d092aa46029c6aad6e88b9c845b6
Jeremy Green <jmgamer12@users.noreply.github.com>
2017-05-03 16:48:20 -0500

    Will folder these in a bit

---
+++ PlayingWithChars.java
@@ -0,0 +1,13 @@
+
+public class PlayingWithChars {
+	public static void main(String[] args) {
+		for (int iRow = 0; iRow <= 10; iRow++) {
+			System.out.printf("%4d", iRow);
+			for (int iCol = 0; iCol < 10; iCol++) {
+				int index = 10 * iRow + iCol;
+				System.out.printf("%5s", (char)index);
+			}
+			System.out.println();
+		}
+	}
+}
